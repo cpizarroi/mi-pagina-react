67dbadd12a1fac2f4cec5e09144b6d91403a6f44
Camila <c.pizarroibaceta@gmail.com>
2024-06-12 21:24:01 -0400

    mi pagina

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Title } from './components/Title'
+import { Button } from './components/Button'
+import { Contador } from './components/Contador'
+import { TextoLibre } from './components/TextoLibre'
+
+function App() {
+  return (
+    <>
+    <Contador/>
+    <TextoLibre/>
+
+
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./title.css"
+
+export function Title(props){
+    return <h1>{props.titulo}</h1>;
+}
+
